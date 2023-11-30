84cbd4a2809fc5dcbd2e70a0c290f3eaa309b20b
Sergio Ojeda <soj3da@gmail.com>
2023-11-30 20:03:34 -0300

    fix: Redirect /login route to filament.admin.auth.login

--- routes/web.php
+++ routes/web.php
@@ -15,18 +15,9 @@ use OpenAI\Laravel\Facades\OpenAI;
 */
 
 
-Route::get('imagen', function () {
-
-    $image = OpenAI::images()->create([
-        'model' => 'dall-e-2',
-        'prompt' => "Please generate an image depicting a psychedelic effect using the 'Liquid Light' technique, infused with vibrant hues of red. The images should evoke an abstract, flowing, and ethereal feel that characterizes the 'Liquid Light' concept, which involves organic interactions of color and light patterns, primarily associated with visual displays from the psychedelic era of the 1960s. That reflects this emotion: Love, Romantic, Affectionate feelings",
-        'size' => '256x256'
-    ]);
-
-    $url = $image->data[0]->url;
-
-    return view('image')->with('url', $url);
-});
+Route::get('/login', function () {
+    return redirect(route('filament.admin.auth.login'));
+})->name('login');
 
 Route::get('{unknown}', fn () => view('app  '))->where('unknown', '^(?!api).*$');
 
