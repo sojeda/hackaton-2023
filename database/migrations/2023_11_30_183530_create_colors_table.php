6bd7f49c2c056a58a9da5c3a08d09ad7036d8358
ezef <ezequiel.flores@lightit.io>
2023-11-30 16:54:36 -0300

    feat: add emotion model and relate with color model

--- database/migrations/2023_11_30_183530_create_colors_table.php
+++ database/migrations/2023_11_30_183530_create_colors_table.php
@@ -6,48 +6,75 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('colors', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('emotion1');
-            $table->string('emotion2');
-            $table->string('emotion3');
-            $table->string('emotion4');
             $table->timestamps();
         });
 
+        Schema::create('emotions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('color_id')->constrained();
+            $table->string('adjectives');
+            $table->timestamps();
+        });
+
+
         Color::create([
             'name' => 'red',
-            'emotion1' => 'Passion, emotional intensity, strong desire',
-            'emotion2' => 'Anger, Rage, Fury',
-            'emotion3' => 'Love, Romantic, Affectionate feelings',
-            'emotion4' => 'Excitement, Enthusiams',
-        ]);
+        ])->emotions()
+            ->createMany([[
+                'adjectives' => 'Passion, emotional intensity, strong desire',
+            ], [
+                'adjectives' => 'Anger, Rage, Fury',
+            ], [
+                'adjectives' => 'Love, Romantic, Affectionate feelings',
+            ], [
+                'adjectives' => 'Excitement, Enthusiams',
+            ]]);
+
         Color::create([
             'name' => 'blue',
-            'emotion1' => 'Calmness, tranquility, relaxation',
-            'emotion2' => 'serenity, sense of inner peace, quiet and harmonious state of mind',
-            'emotion3' => 'Sadness, melancholy, sense of depth and introspection',
-            'emotion4' => 'trust, reliability, sense of security and stability',
-        ]);
+        ])->emotions()
+            ->createMany([[
+                'adjectives' => 'Calmness, tranquility, relaxation',
+            ], [
+                'adjectives' => 'serenity, sense of inner peace, quiet and harmonious state of mind',
+            ], [
+                'adjectives' => 'Sadness, melancholy, sense of depth and introspection',
+            ], [
+                'adjectives' => 'trust, reliability, sense of security and stability',
+            ]]);
+
+
         Color::create([
             'name' => 'yellow',
-            'emotion1' => 'Joy, happiness, cheerful, positive, uplifting mood',
-            'emotion2' => 'optimism, hopefulness, sunny and bright future',
-            'emotion3' => 'Energy, enthusiasm, dynamism, and a zest for life.',
-            'emotion4' => 'Creativity, stimulate mental activity and inspiration',
-        ]);
+        ])->emotions()
+            ->createMany([[
+                'adjectives' => 'Joy, happiness, cheerful, positive, uplifting mood',
+            ], [
+                'adjectives' => 'optimism, hopefulness, sunny and bright future',
+            ], [
+                'adjectives' => 'Energy, enthusiasm, dynamism, and a zest for life.',
+            ], [
+                'adjectives' => 'Creativity, stimulate mental activity and inspiration',
+            ]]);
+
         Color::create([
             'name' => 'green',
-            'emotion1' => 'Balance, Harmony, Symmetry, Stability',
-            'emotion2' => 'Vitality, Growth, Renewal, New Start',
-            'emotion3' => 'Natural, tranquility, peace, calming effect',
-            'emotion4' => 'Jealousy, envy, resentmnet, bitterness',
-        ]);
+        ])->emotions()
+            ->createMany([[
+                'adjectives' => 'Balance, Harmony, Symmetry, Stability',
+            ], [
+                'adjectives' => 'Vitality, Growth, Renewal, New Start',
+            ], [
+                'adjectives' => 'Natural, tranquility, peace, calming effect',
+            ], [
+                'adjectives' => 'Jealousy, envy, resentmnet, bitterness',
+            ]]);
     }
 
     public function down(): void
